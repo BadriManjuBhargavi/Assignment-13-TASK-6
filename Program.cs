0739b2eedee1a923168825fec13a9bc548304446
manju12345 <manju@gmail.com>
2023-07-22 17:23:00 +0530

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BasicCSharpAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 10, 15, 20, 25 };
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            double average = (double)sum / numbers.Length;
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {average}");
+        }
+    }
+}
+
+
